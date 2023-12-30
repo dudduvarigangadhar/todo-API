55366479ed5c2aaf5dde9c2bbb1f5b7085954fbe
gangadhar <141738423+dudduvarigangadhar@users.noreply.github.com>
2023-12-30 14:16:11 +0530

    First API scenario

--- todo_API.js
+++ todo_API.js
@@ -2,64 +2,111 @@ const express = require("express");
 const path = require("path");
 const sqlite3 = require("sqlite3");
 const { open } = require("sqlite");
-const date = require("date-fns");
 const app = express();
 let db = null;
-const initalizeDBAndServer = async () => {
+
+const initialzeDBAndServer = async () => {
   try {
     db = await open({
       filename: path.join(__dirname, "todoApplication.db"),
       driver: sqlite3.Database,
     });
     app.listen(3000, () => {
-      console.log("Server is running at http://localhost:3000/");
+      console.log("Server running at localhost");
     });
   } catch (e) {
-    console.log(`DB Error "{e.message}"`);
+    console.log(`DB Error '${e.message}'`);
     process.exit(1);
   }
 };
-initalizeDBAndServer();
 
-const hasStatusProperty = (requestQuery) => {
-  if (requestQuery.status !== undefined) {
-    return true;
-  } else {
-    response.status(400);
-    response.send("Invalid Todo Status");
-  }
+initialzeDBAndServer();
+const statusProperty = (query) => {
+  return query.status !== undefined;
 };
-const hasPriorityProperty = (requestQuery) => {
-  return requestQuery.priority !== undefined;
+const priorityProperty = (query) => {
+  return query.priority !== undefined;
 };
-const hasCategoryProperty = (requestQuery) => {
-  return requestQuery.category !== undefined;
+const priorityAndStatus = (query) => {
+  return query.priority !== undefined && query.status !== undefined;
 };
-const dbResponseToObject = (todo) => {
+const searchByProperty = (query) => {
+  return query.search_q !== undefined;
+};
+const categoryAndStatus = (query) => {
+  return query.category !== undefined && query.status !== undefined;
+};
+const categoryProperty = (query) => {
+  return query.category !== undefined;
+};
+const categoryAndPriority = (query) => {
+  return query.category !== undefined && query.priority !== undefined;
+};
+const dbResponseToObject = (todoObject) => {
   return {
-    id: todo.id,
-    todo: todo.todo,
-    priority: todo.priority,
-    status: todo.status,
-    Category: todo.category,
-    dueDate: todo.due_date,
+    id: todoObject.id,
+    todo: todoObject.todo,
+    priority: todoObject.priority,
+    status: todoObject.status,
+    category: todoObject.category,
+    dueDate: todoObject.due_date,
   };
 };
 app.get("/todos/", async (request, response) => {
+  //   const selectUser = `SELECT * FROM todo;`;
+  //   const dbResponse = await db.all(selectUser);
+  //   response.send(dbResponse);
   const { status, priority, search_q = "", category } = request.query;
-  let getQuery;
+  let selectQuery;
   let dbResponse;
   switch (true) {
-    case hasStatusProperty(request.query):
-      getQuery = `SELECT * FROM todo WHERE status LIKE '%${status}';`;
-      dbResponse = await db.all(getQuery);
-      response.send(dbResponse.map((todo) => dbResponseToObject(todo)));
+    case statusProperty(request.query):
+      selectQuery = `SELECT * FROM todo WHERE status LIKE '${status}';`;
+      dbResponse = await db.all(selectQuery);
+      response.send(
+        dbResponse.map((todoObject) => dbResponseToObject(todoObject))
+      );
       break;
-    case hasPriorityProperty(request.query):
+    case priorityProperty(request.query):
+      selectQuery = `SELECT * FROM todo WHERE priority LIKE '${priority}';`;
+      dbResponse = await db.all(selectQuery);
+      response.send(
+        dbResponse.map((todoObject) => dbResponseToObject(todoObject))
+      );
       break;
-    case hasCategoryProperty(request.query):
+    case priorityAndStatus(request.query):
+      selectQuery = `SELECT * FROM todo WHERE priority LIKE '${priority}' AND status LIKE '${status}';`;
+      dbResponse = await db.all(selectQuery);
+      response.send(
+        dbResponse.map((todoObject) => dbResponseToObject(todoObject))
+      );
       break;
+    case searchByProperty(request.query):
+      selectQuery = `SELECT * FROM todo WHERE todo LIKE '%${search_q}%';`;
+      dbResponse = await db.all(selectQuery);
+      response.send(
+        dbResponse.map((todoObject) => dbResponseToObject(todoObject))
+      );
+      break;
+    case categoryAndStatus(request.query):
+      selectQuery = `SELECT * FROM todo WHERE category LIKE '${category}' AND status LIKE '${status}';`;
+      dbResponse = await db.all(selectQuery);
+      response.send(
+        dbResponse.map((todoObject) => dbResponseToObject(todoObject))
+      );
+      break;
+    case categoryProperty(request.query):
+      selectQuery = `SELECT * FROM todo WHERE category LIKE '${category}';`;
+      dbResponse = await db.all(selectQuery);
+      response.send(
+        dbResponse.map((todoObject) => dbResponseToObject(todoObject))
+      );
+      break;
+    case categoryAndPriority(request.query):
+      selectQuery = `SELECT * FROM todo WHERE category LIKE '${category}' AND priority LIKE '${priority}';`;
+      dbResponse = await db.all(selectQuery);
+      response.send(
+        dbResponse.map((todoObject) => dbResponseToObject(todoObject))
+      );
   }
 });
-
-GET http://localhost:3000/todos/?status=TO%20DO
